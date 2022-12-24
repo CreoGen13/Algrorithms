8a93c32d611b5bf4108ec41e8acfa25fdacb0fe7
CreoGen13 <creogen.ac@gmail.com>
2022-12-24 13:54:26 +0300

    Lab3

---
+++ Lab2/DynamicArray/DynamicArray.h
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template <typename T>
+class DynamicArray final {
+public:
+    DynamicArray() {
+
+    }
+    DynamicArray(int capacity) {
+
+    }
+    ~DynamicArray() {
+
+    }
+
+    int insert (const T& value) {
+
+    }
+    int insert (int index, const T& value) {
+
+    }
+    void remove (int index) {
+
+    }
+
+    const T& operator[](int index) const {
+
+    }
+    T& operator[](int index) {
+
+    }
+
+    int size() const {
+
+    }
+    //Iterator iterator();
+    //ConstIterator iterator() const;
+    //Iterator reverseIterator();
+    //ConstIterator reverseIterator() const;
+};
+
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include "profileapi.h"
+#include "Sort.h"
+
+double PCFreq = 0.0;
+__int64 CounterStart = 0;
+
+constexpr int kLength = 30;
+constexpr int kRepeats = 100000;
+int arr[kLength];
+
+void countFrequency()
+{
+    LARGE_INTEGER li;
+    if (!QueryPerformanceFrequency(&li))
+        std::cout << "QueryPerformanceFrequency failed!\n";
+
+    PCFreq = double (li.QuadPart) / 1000.0;
+
+    QueryPerformanceCounter(&li);
+    CounterStart = li.QuadPart;
+}
+double getFrequency()
+{
+    LARGE_INTEGER li;
+    QueryPerformanceCounter(&li);
+    return double (li.QuadPart - CounterStart) / PCFreq;
+}
+void unsortArr()
+{
+    for (int i = 0; i < kLength; ++i)
+        arr[i] = kLength - i;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Sort sort;
+
+    for (int i = 1; i <= kLength; ++i)
+    {
+        double insertTime = 0.0, quickTime = 0.0, mixedTime = 0.0;
+
+        for (int k = 0; k < kRepeats; ++k)
+        {
+            // Insertion
+            unsortArr();
+            countFrequency();
+            sort.insertionSort(arr, arr + i - 1, [](int a, int b) { return a < b; });
+            insertTime += getFrequency();
+
+            // Quick
+            unsortArr();
+            countFrequency();
+            sort.quickSort(arr, arr + i - 1, [](int a, int b) { return a < b; });
+            quickTime += getFrequency();
+
+            // Mixed
+            unsortArr();
+            countFrequency();
+            sort.mixedSort(arr, arr + i - 1, [](int a, int b) { return a < b; });
+            mixedTime += getFrequency();
+        }
+        std::cout << "Iteration " << i << ": " << "\t" << insertTime << "\t\t" << quickTime << "\t\t" << mixedTime;
+        //std::cout << i << ";"<< insertTime << ";" << quickTime << ";" << mixedTime << ";";
+        //std::cout << mixedTime;
+        std::cout << std::endl;
+    }
+
     return 0;
 }
